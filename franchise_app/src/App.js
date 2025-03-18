7aa90b25c0e52c2b881f009806dd5a099423484f
rajanalibabaa <rajanalibabaa@gmail.com>
2025-03-18 18:43:18 +0530

    all file changes

--- franchise_app/src/App.js
+++ franchise_app/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+// import {Router,Route,Routes} from "react-router-dom"
+import HomePage from './Pages/HomePage';
 
 function App() {
   return (
-    <div>App</div>
+    <>
+<HomePage/>
+    </>
   )
 }
 
