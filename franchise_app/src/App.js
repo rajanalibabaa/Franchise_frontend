c9bacdacf601ce73b5215239ffc86f415a72dc44
Aravind <bharathiaravind417@gmail.com>
2025-03-19 10:56:22 +0530

    banner adding

--- franchise_app/src/App.js
+++ franchise_app/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from 'react'
+import BannerPage from './Pages/HomePage'
+
 
 function App() {
   return (
-    <h1>wElcome</h1>
-  );
+<>
+<h1>
+  <BannerPage/>
+</h1>
+</>
+  )
 }
 
-export default App;
+export default App
