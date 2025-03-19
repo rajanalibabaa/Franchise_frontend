1a5512391c7ff1b04d020dd8a984391c178cf9e6
ramyaar184 <ramyaar184@gmail.com>
2025-03-19 15:13:55 +0530

    Adding sidebar

--- franchise_app/src/Pages/HomePage.js
+++ franchise_app/src/Pages/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../Compnents/NavBar.jsx'
 import BannerSec from '../Compnents/BannerSec.jsx'
+import Sidebar from '../Compnents/SideBar.jsx'
 
 function HomePage() {
   return (
 <>
-
+<Sidebar/>
 <Navbar/>
 <BannerSec/>
   </>
