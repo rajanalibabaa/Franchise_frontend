6ed460044e0c159bc11c31b2a548ccfbcdd00765
rajanalibabaa <rajanalibabaa@gmail.com>
2025-03-19 14:36:27 +0530

    all Files

--- franchise_app/src/Pages/HomePage.js
+++ franchise_app/src/Pages/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Navbar from '../Compnents/NavBar.js'
-import BannerSec from '../Compnents/BannerSec.js'
+import Navbar from '../Compnents/NavBar.jsx'
+import BannerSec from '../Compnents/BannerSec.jsx'
 
 function HomePage() {
   return (
 <>
+
 <Navbar/>
 <BannerSec/>
   </>
