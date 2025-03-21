7facc436da15d267fde7401235ce15662ecf0a29
rajanalibabaa <rajanalibabaa@gmail.com>
2025-03-21 18:54:09 +0530

    adding page to view

--- franchise_app/src/Pages/HomePage.js
+++ franchise_app/src/Pages/HomePage.js
@@ -3,6 +3,7 @@ import BannerSec from '../Components/BannerSec.jsx';
 import Navbar from '../Components/NavBar.jsx';
 import PopupModal from '../Components/PopupModal.jsx';
 import Footer from '../Components/Footer.jsx';
+import GridViewBrand from '../Components/GridViewBrand.jsx';
 
 function HomePage() {
   const [isPopupVisible, setIsPopupVisible] = useState(false);
@@ -24,6 +25,7 @@ function HomePage() {
       {isPopupVisible && <PopupModal onClose={closePopup} />}
       <Navbar />
       <BannerSec />
+      <GridViewBrand/>
       <Footer/>
     </>
   );
